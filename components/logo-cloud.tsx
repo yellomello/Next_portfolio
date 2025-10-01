156f2bc98d7ec6b0fc8a490d7f84acad37b2ba17
Marc Thomas <marc@example.com>
2025-10-01 15:57:47 +0530

    Logo cloud working

---
+++ components/logo-cloud.tsx
@@ -0,0 +1,127 @@
+import { InfiniteSlider } from '@/components/ui/infinite-slider'
+import { ProgressiveBlur } from '@/components/ui/progressive-blur'
+
+export default function LogoCloud() {
+    return (
+        <section className="bg-background overflow-hidden py-16">
+            <div className="group relative m-auto max-w-7xl px-6">
+                <div className="flex flex-col items-center md:flex-row">
+                    <div className="md:max-w-44 md:border-r md:pr-6">
+                        <p className="text-end text-sm">Data Engineering Skills</p>
+                    </div>
+                    <div className="relative py-6 md:w-[calc(100%-11rem)]">
+                        <InfiniteSlider
+                            speedOnHover={20}
+                            speed={200}
+                            gap={112}>
+                            
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-7 w-fit dark:invert scale-400"
+                                    src="/dockersvg.svg"
+                                    alt="Docker Logo"
+                                    height="20"
+                                    width="auto"
+                                />
+                            </div>
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-7 w-fit  scale-120"
+                                    src="/Apache_Spark_logo.svg.svg"
+                                    alt="Spark Logo"
+                                    height="20"
+                                    width="auto"
+                                />
+                            </div>
+
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-7 w-fit  scale-300"
+                                    src="/twilio.svg"
+                                    alt="Twilio Logo"
+                                    height="16"
+                                    width="auto"
+                                />
+                            </div>
+
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-9 w-auto "
+                                    src="/tensorflow.svg"
+                                    alt="TensorFlow Logo"
+                                    height="16"
+                                    width="auto"
+                                />
+                            </div>
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-9 w-auto scale-300 filter grayscale"
+                                    src="/terra.svg"
+                                    alt="terra Logo"
+                                    height="16"
+                                    width="auto"
+                                />
+                            </div>
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-9 w-auto "
+                                    src="/ts.svg"
+                                    alt="ts Logo"
+                                    height="16"
+                                    width="auto"
+                                />
+                            </div>
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-9 w-auto "
+                                    src="/r.svg"
+                                    alt="r Logo"
+                                    height="16"
+                                    width="auto"
+                                />
+                            </div>
+
+                            
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-7 w-fit dark:invert"
+                                    src="https://html.tailus.io/blocks/customers/github.svg"
+                                    alt="GitHub Logo"
+                                    height="16"
+                                    width="auto"
+                                />
+                            </div>
+                    
+
+                            <div className="flex">
+                                <img
+                                    className="mx-auto h-7 w-fit dark:invert"
+                                    src="https://html.tailus.io/blocks/customers/openai.svg"
+                                    alt="OpenAI Logo"
+                                    height="24"
+                                    width="auto"
+                                />
+                            </div>
+
+
+
+                        </InfiniteSlider>
+
+                        <div className="bg-linear-to-r from-background absolute inset-y-0 left-0 w-20"></div>
+                        <div className="bg-linear-to-l from-background absolute inset-y-0 right-0 w-20"></div>
+                        <ProgressiveBlur
+                            className="pointer-events-none absolute left-0 top-0 h-full w-20"
+                            direction="left"
+                            blurIntensity={1}
+                        />
+                        <ProgressiveBlur
+                            className="pointer-events-none absolute right-0 top-0 h-full w-20"
+                            direction="right"
+                            blurIntensity={1}
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
