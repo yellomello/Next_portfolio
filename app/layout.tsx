10e5ef19407e23d37a98827bf4da74f6ccdb6c4f
Marc Thomas <marc@example.com>
2025-10-07 14:32:49 +0530

    Vercel Page Insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
