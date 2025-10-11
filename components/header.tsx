e8130aaeca2a8fa55c23c5782e72c2af11ba7a98
Marc Thomas <marc@example.com>
2025-10-11 18:35:33 +0530

    Projects updated z-index fixed

--- components/header.tsx
+++ components/header.tsx
@@ -40,7 +40,7 @@ export const HeroHeader = () => {
         <header>
             <nav
                 data-state={menuState && 'active'}
-                className="fixed z-20 w-full px-2">
+                className="fixed z-20 w-full px-2 z-100">
                 <div className={cn('mx-auto mt-2 max-w-6xl px-6 transition-all duration-300 lg:px-12', isScrolled && 'bg-background/50 max-w-4xl rounded-2xl border backdrop-blur-lg lg:px-5')}>
                     <div className="relative flex flex-wrap items-center justify-between gap-6 py-3 lg:gap-0 lg:py-4">
                         <div className="flex w-full justify-between lg:w-auto">
