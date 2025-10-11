55b1256cf70195f48bb202ad9d718aa6bcc28a61
Marc Thomas <marc@example.com>
2025-10-11 18:29:02 +0530

    Projects updated

--- components/expandable-card-demo-standard.tsx
+++ components/expandable-card-demo-standard.tsx
@@ -275,22 +275,13 @@ const cards = [
   {
     description: "",
     title: "Image Feature Extraction and Manipulation",
-    src: "/imagerecognition.png",
+    src: "/coke.png",
     ctaText: "View",
     ctaLink: "https://colab.research.google.com/github/yellomello/Python_Practice-Basics/blob/main/04_Basic_Image_Enhancement_Mathematical_Operations.ipynb",
     content: () => {
       return (
         <p>
-          Lana Del Rey, an iconic American singer-songwriter, is celebrated for
-          her melancholic and cinematic music style. Born Elizabeth Woolridge
-          Grant in New York City, she has captivated audiences worldwide with
-          her haunting voice and introspective lyrics. <br /> <br /> Her songs
-          often explore themes of tragic romance, glamour, and melancholia,
-          drawing inspiration from both contemporary and vintage pop culture.
-          With a career that has seen numerous critically acclaimed albums, Lana
-          Del Rey has established herself as a unique and influential figure in
-          the music industry, earning a dedicated fan base and numerous
-          accolades.
+         
         </p>
       );
     },
